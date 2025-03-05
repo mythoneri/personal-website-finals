55731089a9832a7002648ecd81a62ff1162c7100
Nerimiah Jay Ato <ndato@student.apc.edu.ph>
2025-03-05 04:11:01 +0000

    added home section

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/Home.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -17,30 +17,12 @@ import HelloWorld from './components/Home.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+body {
+  font-family: 'Poppins', sans-serif;
+  background: linear-gradient(to bottom, #183425, #869b8f);
+  color: #EDE6D4;
+  margin: 0;
+  padding: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
--- vue-project/src/components/Home.vue
+++ vue-project/src/components/Home.vue
@@ -1,4 +1,17 @@
 <template>
+
+ <header class="sticky-header">
+        
+        <nav class="nav-link">
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="skills.html">Skills</a>
+            <a href="gallery.html">Gallery</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+    </header>
+
+    
     <div class="text-image-container">
             <div class="text-container">
             <h1>Hi! I'm Nerimiah Jay Ato</h1> 
@@ -10,18 +23,37 @@
              </div>
             <img :src="profileImage" alt="Profile Image"  class="custom-image"/>
         </div>
+
+        <footer>
+        <div class="social-buttons-container">
+            <a href="https://www.facebook.com/nerimiah.jay" target="_blank">
+              <img :src="facebook" alt="Facebook" width="50">
+               </a>
+              <a href="https://www.instagram.com/mytho.ko" target="_blank">
+              <img :src="instagram" alt="Instagram" width="50">
+            </a>
+            <a href="https://www.linkedin.com/in/nerimiah-jay-ato-0ab386288" target="_blank">
+              <img :src="linkedin" alt="LinkedIn" width="50">
+            </a>
+            <a href="https://open.spotify.com/user/nerimiahjayato?si=Hv2cxsbFTjCJySGU_CzeRg" target="_blank">
+              <img :src="spotify" alt="Spotify" width="50">
+            </a>
+        </div>
+         <p>&copy; 2025 Nerimiah Jay Ato. All rights reserved.</p>
+    </footer>
 </template>
 
 <script setup>
 import profileImage from '@/assets/images/profile.jpeg';
+import facebook from '@/assets/images/facebook.jpg';
+import instagram from '@/assets/images/instagram.jpg';
+import linkedin from '@/assets/images/linkedin.jpg';
+import spotify from '@/assets/images/spotify.jpg';
+
+
 </script>
 
 
 <style>
-.apples{
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-    }</style>   
+
+    </style>   
